739026408f65e17e35ceb8fa9a6b4dffa5beec50
Jordan Contract <jordan59@optonline.net>
2019-09-26 17:41:06 -0400

    routerpost

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const {Page} = require("../models");
 const {addPage, main} = require('../views');
+
+function genSlug(title){
+    let regexA = /\s+/g;
+    title = title.replace(regexA, "_");
+    let regexB = /\W/g;
+    title = title.replace(regexB, "");
+    return title;
+}
+
 
 router.get('/', (req, res, next) => {
     res.send(main());
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
+    const page = new Page({
+        title: await genSlug(req.body.title),
+        content: await genSlug(req.body.content)
+      });
+    
+      // make sure we only redirect *after* our save is complete!
+      // note: `.save` returns a promise.
+      try {
+        await page.save();
+        res.redirect('/');
+      } catch (error) { next(error) }
     res.json(req.body);
 });
 
